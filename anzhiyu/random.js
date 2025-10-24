3f8ba83b652dfc6d3a656336650bc79f046cdd2b
shmily-smily-dot <shmily-smily@outlook.com>
2025-10-24 10:54:21 +0800

    Site updated: 2025-10-24 10:54:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/23/欢迎光临/"];function toRandomPost(){
+var posts=["2025/10/24/Introduction/","2025/10/23/欢迎光临/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
